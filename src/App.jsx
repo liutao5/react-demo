d14babddfe2a67fcfe56e6a919a1dab86a6f8dba
knight <liutao_890214@163.com>
2021-10-21 17:11:04 +0800

    #rd-2#add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import TsxDemo from './TsxDemo';
+import {
+  BrowserRouter as Router, Route, Link, Switch,
+} from 'react-router-dom';
+import Dashboard from './pages/dashboard';
+import Userinfo from './pages/userinfo';
 
 const App = () => (
-  <>
-    <TsxDemo name="zwy5" age={27} />
-  </>
+  <Router>
+    <ul>
+      <li><Link to="/dashboard">dashboard</Link></li>
+      <li><Link to="/userinfo">userinfo</Link></li>
+    </ul>
+    <Switch>
+      <Route path="/dashboard"><Dashboard /></Route>
+      <Route path="/userinfo"><Userinfo /></Route>
+    </Switch>
+  </Router>
 
 );
 
